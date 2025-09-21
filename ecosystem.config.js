0d42976449e08cbae533c449f30699be24b1069e
Eduardo Eloi Silva Santos <eduardo.santos@comexport.com.br>
2025-09-20 23:30:49 -0300

    fix(backend): corrige nome do banco de dados na config do pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         DB_HOST: "127.0.0.1",
         DB_USER: "receitas_user",
         DB_PASSWORD: "Receitas@123",
-        DB_NAME: "receitas_milionarias_db",
+        DB_NAME: "receitas_db",
         JWT_SECRET: "segredo_muito_forte_em_producao"
       }
     }
